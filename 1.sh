a0e1644550f7c903a56c192667fd6578a3a96e16
Pablo <pagira@alumnatflorida.es>
2026-01-22 09:27:27 +0100

    añadiendo 1.sh

--- 1.sh
+++ 1.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+contador=0
+totalpar=0
+totalimpar=0
+while read numero
+do
+    resto=$(($numero % 2))
+    if [ $resto -eq 0 ];then
+        echo "Numero pares"
+        contador=$((contador + 1))
+        totalpar=$((totalpar + numero))
+        echo "$contador "-" $numero"
+    fi
+
+done < numeros.txt
+echo "El total de los números pares es: $totalpar"
 
 while read numero
-    do
-        div=$(($numero%2))
-        if [ $div -eq 0 ];then
-            echo "$numero es par"
-        fi
-done < numeros.txt       
+do
+    resto=$(($numero % 2))
+    if [ $resto -ne 0 ];then
+        echo "Numero impar"
+        contador=$((contador + 1))
+        totalimpar=$((totalimpar + numero))
+        echo "$contador "-" $numero"
+    fi
+
+done < numeros.txt
+echo "El total de los numeros impares es: $totalimpar"
